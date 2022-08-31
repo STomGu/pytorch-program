9d662bafa8a801c12b6c1408a4eb041abda741e6
STomGu <107230341+STomGu@users.noreply.github.com>
2022-08-31 09:30:09 -0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+
+data = numpy.loadtxt('wine.csv')
+
+for epoch in range(1000):
+    x, y = data
---
+++ 2.py
@@ -0,0 +1,7 @@
+# training loop
+for epoch in range(1000):
+
+    for i in range(total_batches):
+        x_batch, y_batch = ...
+
+# use dataset and dataloader to load wine.csv
